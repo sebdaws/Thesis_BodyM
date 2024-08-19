d1f4dcf4c43c786b782940cb9dfe264ed7f556d1
sebdaws <ucabs52@ucl.ac.uk>
2024-08-19 11:23:00 +0100

    del

--- measurements/vit_script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-conda activate thesis
-# Run the commands sequentially
-python3 trainer.py --num_epochs 40 --m_inputs --vit
-python3 trainer.py --num_epochs 40 --weight --m_inputs --vit
-python3 trainer.py --num_epochs 40 --gender --m_inputs --vit
-python3 trainer.py --num_epochs 40 --weight --gender --m_inputs --vit
